a7084266c4daf6374acffe67e60ed763e013c8b2
Charlesdigital <charles.easton123@gmail.com>
2021-10-26 01:49:58 -0400

    add without.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,7 @@
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed ${actual} !== ${expected}`);
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+};
---
+++ without.js
@@ -0,0 +1,36 @@
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed ${actual} !== ${expected}`);
+  }
+};
+
+function without(array1, array2) {
+  let resultArray = [];
+  for (let i = 0; i < array1.length; i++) {
+    if (array2.indexOf(array1[i]) === -1) {
+      resultArray.push(array1[i]);
+    }
+  }
+  return resultArray;
+}
+
+assertArraysEqual(eqArrays(without([1, 2, 3], [1]), [2, 3]), true);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
