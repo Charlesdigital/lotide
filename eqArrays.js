de8bb95eaa23aec6da7326eebc6890f86671b986
Charlesdigital <charles.easton123@gmail.com>
2021-10-26 01:27:57 -0400

    add }

--- eqArrays.js
+++ eqArrays.js
@@ -6,13 +6,13 @@ const assertEqual = function (actual, expected) {
 
 //check if each element in the array are the same array1[i] === array2[i]
 function eqArrays(array1, array2) {
-    if(array1.length !== array2.length) {
-        return false
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-    for(let i = 0; i < array1.length; i++) {
-      if(array1[i] !== array2[i]) {
-          return false
-      }
-    }
-    return true;
-
+  }
+  return true;
+}
