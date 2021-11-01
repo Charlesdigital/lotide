38da5f7ffc73b43b3b5f982160fee4eb0b371f5d
Charlesdigital <charles.easton123@gmail.com>
2021-11-01 15:19:13 -0400

    middleTest changes

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,26 @@
-const eqArrays = require("../eqArrays");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-console.log(assertArraysEqual(eqArrays(middle([1, 2, 3], [2])), false)); // => [2]
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns 2 for middle of [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns 3 for middle of [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns 3, 4 for middle of [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
+
+// const eqArrays = require("../eqArrays");
+// const assertArraysEqual = require("../assertArraysEqual");
+// const middle = require("../middle");
+
+// console.log(assertArraysEqual(eqArrays(middle([1, 2, 3], [2])), false)); // => [2]
 // console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
 // console.log(middle([1, 2, 3, 4])); // => [2, 3]
